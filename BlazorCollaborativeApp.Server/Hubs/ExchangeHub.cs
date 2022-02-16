c93cf35e4715b01101d42444befef32c0a86e507
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-02-16 13:20:35 +0100

    ✨Implement hub

---
+++ BlazorCollaborativeApp.Server/Hubs/ExchangeHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorCollaborativeApp.Server.Hubs
+{
+    public class ExchangeHub : Hub
+    {
+        public async Task NotifyChangesAsync(string sId, object data)
+        {
+            await Clients.All.SendAsync("OnChange", sId, data);
+            System.Diagnostics.Debug.WriteLine("A change occured", "OnChange");
+        }
+    }
+}
